94b0c39ddd8fc6cfd367d63c6ad91a10780d05f3
DeepThought77 <gavatron77@outlook.com>
2016-12-01 16:30:35 -0800

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/SImpleStrategy_StartPosition2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/SImpleStrategy_StartPosition2.java
@@ -13,7 +13,7 @@ import org.firstinspires.ftc.teamcode.hardware.HardwareK9bot;
 @Autonomous(name="AutonomousMode2", group="Autonomous")
 public class SimpleStrategy_StartPosition2 extends LinearOpMode {
     /* Declare OpMode members. */
-    HardwareK9bot robot           = new HardwareK9bot();              // Use a K9'shardware
+    HardwareK9bot robot           = new HardwareK9bot();              // Use a K9's hardware
     double          armPosition     = robot.ARM_HOME;                   // Servo safe position
     double          clawPosition    = robot.CLAW_HOME;                  // Servo safe position
     final double    CLAW_SPEED      = 0.01 ;                            // sets rate to move servo
